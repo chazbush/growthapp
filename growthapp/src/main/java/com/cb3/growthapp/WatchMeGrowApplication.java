98c701147eec2fb6a64cc056248424c80ecbbc32
chazbush <chazbush408@yahoo.com>
2018-06-03 00:37:16 -0700

    Add files via upload

---
+++ growthapp/src/main/java/com/cb3/growthapp/WatchMeGrowApplication.java
@@ -0,0 +1,12 @@
+package com.cb3.growthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchMeGrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchMeGrowApplication.class, args);
+	}
+}
---
+++ growthapp/src/test/java/com/cb3/growthapp/WatchMeGrowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cb3.growthapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WatchMeGrowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
