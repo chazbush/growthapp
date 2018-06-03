371407901ef7336ae17566665c08e2872b8ebe19
cb3 <chazbush@gmail.com>
2018-06-03 16:32:47 -0700

    added footer to template.html

---
+++ growthapp/src/main/java/com/cb3/growthapp/model/baby.java
@@ -0,0 +1,61 @@
+package com.cb3.growthapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class baby {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long ID;
+	
+	private String FirstName;
+	private String LastName;
+	private String MiddleInitial;
+	private String Weight;
+	
+	
+	
+	public long getID() {
+		return ID;
+	}
+	public void setID(long iD) {
+		ID = iD;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getMiddleInitial() {
+		return MiddleInitial;
+	}
+	public void setMiddleInitial(String middleInitial) {
+		MiddleInitial = middleInitial;
+	}
+	public String getWeight() {
+		return Weight;
+	}
+	public void setWeight(String weight) {
+		Weight = weight;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
